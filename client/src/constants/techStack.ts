eb85f9547236d4af4eadac8717403d75c44560c2
ezemastro <mastropietroezequiel@gmail.com>
2026-02-14 22:56:31 +0000

    Add list of tech stack and its icons

---
+++ client/src/constants/techStack.ts
@@ -0,0 +1,36 @@
+interface Tech {
+  name: string;
+  icon: string;
+}
+const ICON_BASE_PATH = "/icons/";
+
+export const TECH_STACK = [
+  {
+    name: "React",
+    icon: ICON_BASE_PATH + "reactjs.svg",
+  },
+  {
+    name: "Node.js",
+    icon: ICON_BASE_PATH + "nodejs.svg",
+  },
+  {
+    name: "TypeScript",
+    icon: ICON_BASE_PATH + "typescript.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: ICON_BASE_PATH + "tailwindcss.svg",
+  },
+  {
+    name: "Docker",
+    icon: ICON_BASE_PATH + "docker.svg",
+  },
+  {
+    name: "npm",
+    icon: ICON_BASE_PATH + "npm.svg",
+  },
+  {
+    name: "HTML5",
+    icon: ICON_BASE_PATH + "html5.svg",
+  },
+];
